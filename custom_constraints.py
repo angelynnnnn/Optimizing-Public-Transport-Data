c64456c393f50058094e9a791150b4affde1bdd9
LeeZhengXianOwen <owenlee888@gmail.com>
2024-11-05 15:42:31 +0800

    Updated valid_route constrant and added new valid_ranking constraint

--- custom_constraints.py
+++ custom_constraints.py
@@ -26,7 +26,7 @@ def valid_route(column_names, data):
             if end_index == 0: # for routes that loop back
                 end_index = len(route)
             
-            return start_index < end_index
+            return start_index < end_index and start != end
         
         return False
     
@@ -37,13 +37,20 @@ def valid_time(column_names, data):
         time_str = row[column_names[0]]
         time = pd.to_datetime(time_str, format='%H:%M:%S').time()
 
-        if (pd.to_datetime('07:00:00', format='%H:%M:%S').time() <= time)  or (time <= pd.to_datetime('23:00:00', format='%H:%M:%S').time()):
+        if (pd.to_datetime('07:00:00', format='%H:%M:%S').time() <= time <= pd.to_datetime('23:00:00', format='%H:%M:%S').time()):
             return True
         else:
             return False
 
     return data.apply(check_time, axis=1)
 
+def valid_ranking(column_names, data):
+    def check_ranking(row):
+        ranks = row[column_names].tolist()
+        return len(ranks) == len(set(ranks))
+    
+    return data.apply(check_ranking, axis=1)
+        
 def transform(column_names, data):
     return data
 
@@ -58,6 +65,12 @@ BusStopsCheck = create_custom_constraint_class(
 
 TimeCheck = create_custom_constraint_class(
     is_valid_fn = valid_time,
+    transform_fn = transform,
+    reverse_transform_fn = reverse_transform
+)
+
+RankingCheck = create_custom_constraint_class(
+    is_valid_fn = valid_ranking,
     transform_fn = transform,
     reverse_transform_fn = reverse_transform
 )
